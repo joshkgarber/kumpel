5747a0c2c5fe2312a54690aea6596aee12407683
jkgarber <joshuakgarber@outlook.com>
2025-05-21 06:31:18 +0200

    bugfix for multi-line answers

--- jkgflashcards/__main__.py
+++ jkgflashcards/__main__.py
@@ -107,7 +107,7 @@ def run_session(spec):
                             answer = get_recorded_answer()
                         elif input_mode == 'type':
                             answer = ''
-                            while not answer and not answer.isspace():
+                            while not answer and answer.isspace():
                                 answer = input('Your answer: ')
                         print('\nChecking answer... ', end='\a')
                         ai_response = ai_checker(answer, front, back)
