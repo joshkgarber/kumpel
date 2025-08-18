22a02dc12534d3f01b1744cb578ebe2c0bf08c1e
JKG <69854236+joshkgarber@users.noreply.github.com>
2025-08-18 10:36:36 +0200

    user input for a style or genre (#4)

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def main():
     level = get_german_level()
     os.system("clear")
     topic = get_user_topic()
-    print(f"topic: {topic}")
+    os.system("clear")
+    style = get_particular_style()
+    print(f"style: {style}")
 
 
 def get_german_level():
@@ -38,6 +40,31 @@ def get_user_topic():
         return None
     message = """
 Which topics and/or themes would you like to cover in the session?
+
+Respond in one line (140 characters max)."""
+    pattern = r"^.{1,140}$"
+    invalid_message = "Your answer must be in one line and 1 to 140 characters long."
+    user_input = get_user_input(message, pattern, invalid_message)
+    return user_input
+
+
+def get_particular_style():
+    message = """Is there any particular style or genre you would like the text be in?\n
+For example:
+- Casual/everyday street talk
+- Formal language in a professional setting
+- Sci-Fi, futuristic, and outer-space
+- Medieval fairytales and poetry
+- Ancient mythology and biblical\n
+1. Yes -> I'll decide.
+2. No -> the LLM can decide."""
+    pattern = r"^[1, 2]$"
+    invalid_message = "Respond with 1 for yes or 2 for no."
+    user_input = get_user_input(message, pattern, invalid_message)
+    if user_input == "2":
+        return None
+    message = """
+Which style or genre would you like to request?
 
 Respond in one line (140 characters max)."""
     pattern = r"^.{1,140}$"
