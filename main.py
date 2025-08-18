1c1587f5b32b3bd8c29f52a3ddcecaef6870f717
Josh Garber <josh@jkgarber.com>
2025-08-18 09:53:05 +0200

    first question or get_user_topic

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     print("Hello from Kumpel!\n")
     level = get_german_level()
     os.system("clear")
-    # topic = get_topic()
+    topic = get_user_topic()
 
 
 def get_german_level():
@@ -21,8 +21,17 @@ def get_german_level():
 7. C1
 8. C2\n
 Respond with the number for your selection."""
-    pattern = r"^[1,2,3,4,5,6]$"
+    pattern = r"^[1,2,3,4,5,6,7,8]$"
     invalid_message = "Answer must be a number from 1 to 8."
+    return get_user_input(message, pattern, invalid_message)
+
+
+def get_user_topic():
+    message = """Are there any particular topics or themes you would like to focus on?\n
+1. Yes -> I'll decide.
+2. No -> the LLM can decide."""
+    pattern = r"^[1, 2]$"
+    invalid_message = "Respond with 1 for yes or 2 for no."
     return get_user_input(message, pattern, invalid_message)
 
 
