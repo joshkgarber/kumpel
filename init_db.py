d88b7cf1aca0bbadcb45e45187306ff92cf856f3
Josh Garber <josh@jkgarber.com>
2025-08-23 14:16:47 +0200

    tidy schema script

--- init_db.py
+++ init_db.py
@@ -5,17 +5,17 @@ from db import get_db
 def main():
     db = get_db()
     db.executescript("""
-        BEGIN;
-        DROP TABLE IF EXISTS story;
-        CREATE TABLE story (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            level TEXT NOT NULL,
-            topic TEXT
-            style TEXT,
-            jsonstring TEXT NOT NULL
-        );
-        COMMIT;
+BEGIN;
+DROP TABLE IF EXISTS story;
+CREATE TABLE story (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    level TEXT NOT NULL,
+    topic TEXT
+    style TEXT,
+    jsonstring TEXT NOT NULL
+);
+COMMIT;
     """)
     db.close()
     print("Initialized the database.")
