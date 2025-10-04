0275b2c11058a20fe1de6ba560475345bb3645ff
Josh Garber <josh@jkgarber.com>
2025-10-04 11:04:16 +0200

    figuring out testing

--- test_db.py
+++ test_db.py
@@ -1,13 +1,20 @@
 import unittest
-from db import save_story
+import tempfile
+from db import init_db, get_db, save_story
 from main import Story, StorySentence 
 
 
 class TestDB(unittest.TestCase):
+    def setUp():
+        db_fd, db_path = tempfile.mkstemp()
+        init_db("db_path")
+
+    def tearDown():
+        os.close(db_fd)
+        os.unlink(db_path)
     def test_save_story(self):
         sentences = [StorySentence(id=1, german="Hallo!", english="Hello!")]
         story_name = "Test Story"
         story_content = Story(story_name=story_name, sentences=sentences)
         story = dict(content=story_content, level=1, topic=None, style=None, model="gemini-2.5-flash-lite")
         save_story(story)
-
